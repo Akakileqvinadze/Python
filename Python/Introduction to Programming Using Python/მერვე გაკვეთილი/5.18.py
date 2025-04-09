d2deffe8bc81a684247f9f38a5d885b8eb20b8f9
Akakileqvinadze <akakileqvinadze@yahoo.com>
2025-04-09 11:33:52 +0400

    Add files via upload

---
+++ Python/Introduction to Programming Using Python/მერვე გაკვეთილი/5.18.py
@@ -0,0 +1,13 @@
+number = int(input("sheiyvanet ricxvi: "))
+while number % 2 == 0:
+    if number % 2 == 0:
+        number = number / 2
+        print("2", end=",")
+while number % 3 == 0:
+    if number % 3 == 0:
+        number = number / 3
+        print("3", end=",")
+while number % 5 == 0:
+    if number % 5 == 0:
+        number = number / 5
+        print("5", end=",")
